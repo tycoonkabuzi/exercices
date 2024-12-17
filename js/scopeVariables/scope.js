fbc00718a962d4c99a17581a5036dbf8bc754b2a
Tycoon Kabuzi <kabuzitycoon@gmail.com>
2024-12-17 13:43:31 +0100

    Destructurisation done

---
+++ js/scopeVariables/scope.js
@@ -0,0 +1,9 @@
+/*------------------------destructurisation---------------------------------------
+
+let names = ["Krystian", "Ania", "Luc", "Adam"];
+let [name1, name2, name3] = names;
+console.log(name3);
+
+
+------------------------destructurisation---------------------------------------
+*/
